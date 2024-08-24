69433f452703f079c31ef913d6b5a2ff9bc084bc
zizhou <hangli_work@163.com>
2024-08-24 09:55:02 +0800

    part test, save

--- run_part.sh
+++ run_part.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 # 定义常量
-BATCH_SIZE=64
+BATCH_SIZE=32
 HIDDEN_SIZE=64
-SEQ_LENGTH=128
-NUM_LAYERS=6
+SEQ_LENGTH=256
+NUM_LAYERS=4
 PATIENCE=10
 
 cd /root/autodl-tmp/project/
 
-for MISSING_RATE in 0.2 0.4 0.6 0.8; do
-    for MAX_MISSING_RATE in 0.10 0.15 0.20 0.25; do
+for MISSING_RATE in 0.6 0.8; do
+    for MAX_MISSING_RATE in 0.5 0.6; do
         echo "Running with missing-rate=$MISSING_RATE and max-missing-rate=$MAX_MISSING_RATE"
 
         # 计算 SEQ_LENGTH
-        PRODUCT=$(echo "scale=2; $MISSING_RATE * $MAX_MISSING_RATE * 1160" | bc)
+#        PRODUCT=$(echo "scale=2; $MISSING_RATE * $MAX_MISSING_RATE * 1160" | bc)
 
-        # 输出计算的 PRODUCT 值
-        echo "Calculated PRODUCT=$PRODUCT"
+        # # 输出计算的 PRODUCT 值
+        # echo "Calculated PRODUCT=$PRODUCT"
 
-        if (( $(echo "$PRODUCT <= 50" | awk '{print ($1 <= 50)}') )); then
-            SEQ_LENGTH=64
-            HIDDEN_SIZE=64
-        elif (( $(echo "$PRODUCT <= 85" | awk '{print ($1 <= 85)}') )); then
-            SEQ_LENGTH=128
-            HIDDEN_SIZE=128
-        elif (( $(echo "$PRODUCT <= 180" | awk '{print ($1 <= 180)}') )); then
-            SEQ_LENGTH=256
-            HIDDEN_SIZE=256
-        else
-            SEQ_LENGTH=360
-            HIDDEN_SIZE=360
-        fi
+        # if (( $(echo "$PRODUCT <= 50" | awk '{print ($1 <= 50)}') )); then
+        #     SEQ_LENGTH=64
+        #     HIDDEN_SIZE=64
+        # elif (( $(echo "$PRODUCT <= 85" | awk '{print ($1 <= 85)}') )); then
+        #     SEQ_LENGTH=128
+        #     HIDDEN_SIZE=128
+        # elif (( $(echo "$PRODUCT <= 180" | awk '{print ($1 <= 180)}') )); then
+        #     SEQ_LENGTH=256
+        #     HIDDEN_SIZE=256
+        # else
+        #     SEQ_LENGTH=360
+        #     HIDDEN_SIZE=360
+        # fi
 
-        echo "Using SEQ_LENGTH=$SEQ_LENGTH"
+#        echo "Using SEQ_LENGTH=$SEQ_LENGTH"
 
         # 执行 Python 程序
         python /root/autodl-tmp/project/main.py \
